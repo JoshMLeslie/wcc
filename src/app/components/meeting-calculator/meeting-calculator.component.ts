0a5e3c4487b7bc9e75538aa1fa215ff7c3a524c1
Josh Leslie <hello@joshmleslie.com>
2023-10-24 13:26:58 -0400

    add basic list of added locations

--- src/app/components/meeting-calculator/meeting-calculator.component.ts
+++ src/app/components/meeting-calculator/meeting-calculator.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnChanges, SimpleChanges } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormArray,
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
 import { DateTime } from 'luxon';
 import { pairwise } from 'rxjs';
+import { Location } from 'src/app/interface/location';
 import { TimeZone } from 'src/app/interface/time-zone';
 import { TimeService } from 'src/app/service/time.service';
 
@@ -18,27 +24,52 @@ interface MeetingForm {
 export class MeetingCalculatorComponent {
   readonly timeZone = TimeZone;
   utc = this.timeService.formatUTCOffset({ zone: this.timeService.localZone });
-  form = new FormGroup({
+  startTimeForm = new FormGroup({
     selectedZone: new FormControl(this.timeService.localZone),
     selectedDateTime: new FormControl(DateTime.now()),
   });
+  addLocationForm = new FormControl();
+  locations: DateTime[] = [];
 
-  get time(): string {
-    return this.form.get('selectedDateTime')?.value?.toLocaleString(DateTime.DATETIME_SHORT) || '';
+  get mainTime(): string {
+    return (
+      this.startTimeForm
+        .get('selectedDateTime')
+        ?.value?.toLocaleString(DateTime.DATETIME_SHORT) || ''
+    );
   }
 
   constructor(public timeService: TimeService) {
-    this.form.valueChanges.pipe(pairwise()).subscribe(([previous, latest]) => {
-      const { selectedZone: prevZone } = previous as Required<MeetingForm>;
-      const { selectedZone: zone, selectedDateTime } =
-        latest as Required<MeetingForm>;
+    this.startTimeForm.valueChanges
+      .pipe(pairwise())
+      .subscribe(([previous, latest]) => {
+        const { selectedZone: prevZone } = previous as Required<MeetingForm>;
+        const { selectedZone: zone, selectedDateTime } =
+          latest as Required<MeetingForm>;
 
-      if (prevZone !== zone) {
-        this.form.patchValue({
-          selectedDateTime: selectedDateTime.setZone(zone),
-        });
-        this.utc = this.timeService.formatUTCOffset({ zone });
-      }
-    });
+        if (prevZone !== zone) {
+          this.startTimeForm.patchValue({
+            selectedDateTime: selectedDateTime.setZone(zone),
+          });
+          this.utc = this.timeService.formatUTCOffset({ zone });
+        }
+      });
+  }
+
+  addClocks() {
+
+  }
+  
+  addLocation() {
+    const newLoc = DateTime.local({ zone: this.addLocationForm.value });
+    this.locations.push(newLoc);
+  }
+
+  removeLocation(index: number): void {
+    this.locations.splice(index, 1);
+  }
+
+  formatLocationTime(location: DateTime): string {
+    return location.toLocaleString(DateTime.DATETIME_SHORT)
   }
 }
